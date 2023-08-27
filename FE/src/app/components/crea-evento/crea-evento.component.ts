c9f2f90cfaddac9511bc5476c2b7c6870a578d3c
Fabrizio <fabriziofontana02@gmail.com>
2023-08-27 10:40:04 +0200

    bozza form crea evento

--- FE/src/app/components/crea-evento/crea-evento.component.ts
+++ FE/src/app/components/crea-evento/crea-evento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-crea-evento',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./crea-evento.component.css']
 })
 export class CreaEventoComponent {
+  protected titolo: string = '';
+  protected descrizione: string = '';
+  protected dataInizio: string = '';
+  protected dataFine: string = '';
 
+  constructor(private toastr: ToastrService) { }
+
+  //Inizializza i campi del form
+  resetForm(): void {
+    this.titolo = '';
+    this.descrizione = '';
+    this.dataInizio = '';
+    this.dataFine = '';
+    this.toastr.info('Dati resettati');
+  }
+
+  creaEvento(): void {
+    //TODO controllare che tutti i campi siano stati compilati (con bordo rosso)
+    console.log(this.titolo, this.descrizione, this.dataInizio, this.dataFine);
+  }
 }
